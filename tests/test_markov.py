40e909c00d35e15e4adccec00938ea5c62d392e3
Victor <victor.gruselius@gmail.com>
2024-06-28 08:01:53 +0200

    Hm

--- tests/test_markov.py
+++ tests/test_markov.py
@@ -8,7 +8,7 @@ from numpyro.infer import Predictive, NUTS, MCMC
 from numpyro_sts import DiscreteMarkovChain
 
 
-def markov_switching_regime(n: int, x: np.ndarray = None):
+def markov_switching_regime(n: int, y: np.ndarray = None):
     # markov
     with numpyro.plate("num_states", 2):
         transition_matrix = numpyro.sample("transition_matrix", Dirichlet(np.ones(2)))
@@ -17,34 +17,36 @@ def markov_switching_regime(n: int, x: np.ndarray = None):
     initial_value = numpyro.sample("v_0", Categorical(initial_dist))
 
     markov_model = DiscreteMarkovChain(n, transition_matrix, initial_value)
-    state = numpyro.sample("x", markov_model, infer={"enumerate": "parallel"})
+    x = numpyro.sample("x", markov_model, infer={"enumerate": "parallel"})
 
     # obs
     good = numpyro.sample("good", HalfNormal())
     bad = -numpyro.sample("bad", HalfNormal())
-
     mu = jnp.stack([bad, good])
+
     sigma = numpyro.sample("sigma", HalfNormal())
 
     with numpyro.plate("num_time", n):
-        y = numpyro.sample("y", Normal(mu[state], sigma), obs=x)
+        y = numpyro.sample("y", Normal(mu[x], sigma), obs=y)
 
 
 
 def test_markov():
     predictive = Predictive(markov_switching_regime, num_samples=1)
     key = PRNGKey(0)
-    samples = predictive(key, 500)
+    samples = predictive(key, 1_500)
 
     y = np.array(samples["y"].squeeze(0))
 
     kernel = NUTS(markov_switching_regime)
-    mcmc = MCMC(kernel, num_samples=1_000, num_warmup=2_000)
+    mcmc = MCMC(kernel, num_samples=1_000, num_warmup=5_000)
     mcmc.run(key, y.shape[0], y)
 
-    # TODO: test values
+    mcmc.print_summary()
 
     posterior_samples = mcmc.get_samples(group_by_chain=False)
+    predictive = Predictive(markov_switching_regime, posterior_samples=posterior_samples, return_sites=["x"])
+    preds = predictive(key, y.shape[0], y)
 
-    for name, s in samples.keys():
+    for name, s in samples.items():
         print()
