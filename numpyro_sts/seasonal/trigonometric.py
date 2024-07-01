33fbb5ee5e0352804ce0960d0533c27400bc287e
Victor <victor.gruselius@gmail.com>
2024-07-01 09:35:45 +0200

    initial commit

---
+++ numpyro_sts/seasonal/trigonometric.py
@@ -0,0 +1,21 @@
+from math import ceil
+
+import jax.numpy as jnp
+from numpy.typing import ArrayLike
+
+from ..base import LinearTimeseries
+
+
+class TrigonometricSeasonality(LinearTimeseries):
+    """
+    Represents a seasonal component by means of a trigonometric series TODO: math
+    """
+
+    def __init__(self, n: int, num_seasons: int, std: ArrayLike, initial_value: ArrayLike):
+        half_seasons = ceil(num_seasons / 2)
+
+        lamdas = 2 * jnp.pi * jnp.arange(1, half_seasons) / num_seasons
+
+
+        super().__init__(n, offset, matrix, std, initial_value)
+
