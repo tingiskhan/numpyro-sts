50534a114d9dcefeab38b00515dc36ea03ced6a8
Victor <victor.gruselius@gmail.com>
2024-07-02 08:19:14 +0200

    Correct

--- numpyro_sts/periodic/trigonometric.py
+++ numpyro_sts/periodic/trigonometric.py
@@ -1,4 +1,4 @@
-from math import ceil
+from math import floor
 
 import jax.numpy as jnp
 from numpy.typing import ArrayLike
@@ -16,7 +16,7 @@ class TrigonometricSeasonality(Cyclical):
     """
 
     def __init__(self, n: int, num_seasons: int, std: ArrayLike, initial_value: ArrayLike):
-        half_seasons = ceil(num_seasons / 2)
+        half_seasons = floor(num_seasons / 2)
         lamdas = 2 * jnp.pi * jnp.arange(1, half_seasons + 1) / num_seasons
 
         super().__init__(n, lamdas, std, initial_value)
