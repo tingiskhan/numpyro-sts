d0697bd90da57eb50f7d048d4450bf6843c2de30
Victor <victor.gruselius@gmail.com>
2024-07-04 09:31:59 +0200

    Temp

---
+++ tests/test_union.py
@@ -0,0 +1,18 @@
+from numpyro_sts import AutoRegressive, periodic, LocalLinearTrend
+import numpy as np
+import jax.random as jrnd
+
+
+def test_union():
+    n = 100
+
+    llt = LocalLinearTrend(n, np.array([0.05, 1e-3]), np.zeros(2))
+    ar = AutoRegressive(n, 0.99, 0.05, 1)
+    seasonal = periodic.SeasonalSeries(n, 5, 0.05, np.zeros(4))
+
+    combined = llt.union(ar).union(seasonal)
+
+    key = jrnd.PRNGKey(123)
+    samples = combined.sample(key)
+
+    assert samples.shape == combined.event_shape
